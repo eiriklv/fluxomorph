0b448f8ed299d174251e986a355dfb50c9982556
Eirik Vullum <evullum@gmail.com>
2015-02-24 02:24:22 +0100

    rearrange/group for readability

--- lib.js
+++ lib.js
@@ -28,20 +28,34 @@ Flux.prototype.registerStore = function(name, storeDefinition) {
   this.Stores[name] = Store(storeDefinition, this.context);
 };
 
-Flux.prototype.registerAction = function(name, actionCreator) {
-  this.Actions[name] = actionCreator(this.context);
-};
-
 Flux.prototype.registerStores = function(storeDefinitions) {
   for (let store in storeDefinitions) {
     this.registerStore(store, storeDefinitions[store]);
   }
 };
 
+Flux.prototype.registerAction = function(name, actionCreator) {
+  this.Actions[name] = actionCreator(this.context);
+};
+
 Flux.prototype.registerActions = function(actionCreators) {
   for (let creator in actionCreators) {
     this.registerAction(creator, actionCreators[creator]);
   }
+};
+
+Flux.prototype.registerSocketActors = function(socket, eventActorDefinitions) {
+  for (let event in eventActorDefinitions) {
+    this.registerSocketActor(socket, event, eventActorDefinitions[event], this.context);
+  }
+};
+
+Flux.prototype.registerSocketActor = function(socket, event, eventActor) {
+  socket.on(event, eventActor(this.context));
+};
+
+Flux.prototype.addToContext = function(name, obj) {
+  this.context[name] = obj;
 };
 
 Flux.prototype.dehydrate = function() {
@@ -55,20 +69,6 @@ Flux.prototype.rehydrate = function(appState) {
   for (let Store in this.Stores) {
     this.Stores[Store].replaceState(appState[Store] || {})
   }
-};
-
-Flux.prototype.addToContext = function(name, obj) {
-  this.context[name] = obj;
-};
-
-Flux.prototype.registerSocketActors = function(socket, eventActorDefinitions) {
-  for (let event in eventActorDefinitions) {
-    this.registerSocketActor(socket, event, eventActorDefinitions[event], this.context);
-  }
-};
-
-Flux.prototype.registerSocketActor = function(socket, event, eventActor) {
-  socket.on(event, eventActor(this.context));
 };
 
 Flux.StateMixin = StateMixin;
